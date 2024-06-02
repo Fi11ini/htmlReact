e76b71c5539972f8472974b9b33d5e3c3f5cfc64
Andrey <andreymdm@gmail.com>
2024-06-02 18:08:06 +0300

    Add Main blocks

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -3,7 +3,17 @@ import React from 'react';
 export const Main = () => {
     return (
         <div>
-
+            <div>
+                <span>
+                    Hi There
+                </span>
+                <h2>
+                    I am Svetlada Dyblo
+                </h2>
+                <h1>
+                    A Web Developer
+                </h1>
+            </div>
         </div>
     );
 };
