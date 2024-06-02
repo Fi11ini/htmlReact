5a76823aca4671cd098627aeadceda44b7408ccb
Andrey <andreymdm@gmail.com>
2024-06-02 18:09:38 +0300

    Revert "Add Main blocks"
    This reverts commit e76b71c5539972f8472974b9b33d5e3c3f5cfc64.

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -3,17 +3,7 @@ import React from 'react';
 export const Main = () => {
     return (
         <div>
-            <div>
-                <span>
-                    Hi There
-                </span>
-                <h2>
-                    I am Svetlada Dyblo
-                </h2>
-                <h1>
-                    A Web Developer
-                </h1>
-            </div>
+
         </div>
     );
 };
