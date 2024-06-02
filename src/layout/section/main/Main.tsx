d6acdbdc28e59f280f23d2b66c6ada66d7b422aa
Andrey <andreymdm@gmail.com>
2024-06-02 18:41:33 +0300

    Test GIT

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from "styled-components";
+import photo from '../../../assets/images/photo.jpg'
 
 export const Main = () => {
     return (
@@ -13,8 +15,17 @@ export const Main = () => {
                 <h1>
                     A Web Developer
                 </h1>
+
+                <Photo src={photo} alt=""/>
+п
             </div>
         </div>
     );
 };
 
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+`
